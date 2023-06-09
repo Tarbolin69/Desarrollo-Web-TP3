7e44fbbf3a02ee40ff6c8a2c164439819cd3b755
Tarbolin69 <tarbolin69@gmail.com>
2023-06-09 20:54:12 -0300

    primer push

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function jesusHabla() {
+    var things = ['Milanesa', 'Mirta Legrand tiene la sangre de los angeles', 'Milei Presidente', 'Judas era de River', 'Estan enterrados bajo la bombonera'];
+    var thing = things[Math.floor(Math.random()*things.length)];
+    alert('Jesus dice: ' + thing);
+}
