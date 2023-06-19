07ca382c280b808a4feb453be88b8b97d89ea586
Tarbolin69 <tarbolin69@gmail.com>
2023-06-19 02:45:58 -0300

    voluntarios, campañas y adopciones

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-function jesusHabla() {
-    var things = ['Milanesa', 'Mirta Legrand tiene la sangre de los angeles', 'Milei Presidente', 'Judas era de River', 'Estan enterrados bajo la bombonera'];
-    var thing = things[Math.floor(Math.random()*things.length)];
-    alert('Jesus dice: ' + thing);
+const dialog = document.getElementById("formulario")
+
+function mostrarLoginDialog(){
+    dialog.showModal()
+}
+
+function cerrarLoginDialog(){
+    dialog.close()
 }
