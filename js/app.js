936a46a73509c07b6e2d7ea33bab175a37e82507
Tarbolin69 <tarbolin69@gmail.com>
2023-06-20 19:48:05 -0300

    elemento js deprecado

--- js/app.js
+++ js/app.js
@@ -9,11 +9,3 @@ function mostrarLoginDialog(){
 function cerrarLoginDialog(){
     dialog.close()
 }
-
-// Header Responsive
-function openNav() {
-    document.getElementById("sidenav").style.width = "50%";
-}
-function closeNav() {
-    document.getElementById("sidenav").style.width = "0%";
-}
